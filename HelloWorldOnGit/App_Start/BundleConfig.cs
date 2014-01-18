b850182087c4fe1915ccd44497f18d6f9998afae
ShrikantN <shrikantnepte@gmail.com>
2014-01-18 13:43:14 +0530

    Commiting for testing

--- HelloWorldOnGit/App_Start/BundleConfig.cs
+++ HelloWorldOnGit/App_Start/BundleConfig.cs
@@ -19,6 +19,7 @@ namespace HelloWorldOnGit
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));

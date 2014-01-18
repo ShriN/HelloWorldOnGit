ae37c742d0ec099461d3695c07af19a0feeea0ea
ShrikantN <shrikantnepte@gmail.com>
2014-01-18 13:41:46 +0530

    Added new projecct from VS 2013

---
+++ HelloWorldOnGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorldOnGit.Startup))]
+namespace HelloWorldOnGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
